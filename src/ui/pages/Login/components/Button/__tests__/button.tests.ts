2d494f2a2c9277db263b7fe41129afa26ac878ed
kino6052 <113693383+nino-ux@users.noreply.github.com>
2024-12-27 11:19:26 +0300

    Refactor

--- src/ui/pages/Login/components/Button/__tests__/button.tests.ts
+++ src/ui/pages/Login/components/Button/__tests__/button.tests.ts
@@ -1,39 +1,11 @@
-import { EConstant } from "../../../../../../constants";
-import { EPage, TAppState, TSetState } from "../../../../../types";
-import { TLogin, TLoginResponse } from "../../../types";
-import { getMapStateToProps } from "../logic";
-import { StateManager } from "./stateManager";
+import { setup } from "./utils";
 
 describe("button test", () => {
   it("should login", async () => {
-    const login: TLogin = jest.fn().mockImplementation(() =>
-      Promise.resolve({
-        ontology: {
-          [EConstant.Root]: {
-            id: EConstant.Root,
-            isCollapsed: false,
-            isMenuOpen: false,
-            successors: [],
-            text: "",
-          },
-        },
-      } satisfies TLoginResponse)
-    );
+    // Setup
+    const { login, mapStateToButtonProps, stateManager } = setup();
 
-    const initialState: TAppState = {
-      pageType: EPage.Login,
-      pageState: {
-        isLoading: false,
-        message: "",
-      },
-    };
-
-    const stateManager = new StateManager<TAppState>(initialState);
-
-    const mapStateToButtonProps = getMapStateToProps({
-      login,
-    });
-
+    // Core
     const props = mapStateToButtonProps(
       stateManager.getState(),
       stateManager.setState.bind(stateManager)
---
+++ src/ui/pages/Login/components/Button/__tests__/utils.ts
@@ -0,0 +1,37 @@
+import { EConstant } from "../../../../../../constants";
+import { EPage, TAppState } from "../../../../../types";
+import { TLogin, TLoginResponse } from "../../../types";
+import { getMapStateToProps } from "../logic";
+import { StateManager } from "./stateManager";
+
+export const setup = () => {
+  const login: TLogin = jest.fn().mockImplementation(() =>
+    Promise.resolve({
+      ontology: {
+        [EConstant.Root]: {
+          id: EConstant.Root,
+          isCollapsed: false,
+          isMenuOpen: false,
+          successors: [],
+          text: "",
+        },
+      },
+    } satisfies TLoginResponse)
+  );
+
+  const initialState: TAppState = {
+    pageType: EPage.Login,
+    pageState: {
+      isLoading: false,
+      message: "",
+    },
+  };
+
+  const stateManager = new StateManager<TAppState>(initialState);
+
+  const mapStateToButtonProps = getMapStateToProps({
+    login,
+  });
+
+  return { login, stateManager, mapStateToButtonProps };
+};
